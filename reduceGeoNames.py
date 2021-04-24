793933e1f2ce72bbc30e71c238e3e55b03340de6
Programmer100th <48069809+Programmer100th@users.noreply.github.com>
2021-04-24 11:18:29 +0200

    Updated python script and corresponding geonames file

--- reduceGeoNames.py
+++ reduceGeoNames.py
@@ -18,11 +18,13 @@ def reduceGeoNames(nameFileIn, nameFileOut):
     tot_pal = 0
     tot_pyr = 0
     tot_hsts = 0
+    tot_amth = 0
+    tot_bldg = 0
     
     for row in file_in:
         row = row.strip().split('\t')
         tot_rows = tot_rows + 1
-        if(row[7] == 'MNMT' or row[7] == 'CSTL' or row[7] == 'ANS' or row[7] == 'CH' or row[7] == 'HSTS' or row[7] == 'MUS' or row[7] == 'PAL' or row[7] == 'PYR'): #monuments
+        if(row[7] == 'MNMT' or row[7] == 'CSTL' or row[7] == 'ANS' or row[7] == 'CH' or row[7] == 'HSTS' or row[7] == 'MUS' or row[7] == 'PAL' or row[7] == 'PYR' or row[7] == 'AMTH' or row[7] == 'BLDG'): #monuments
             
             #print(row)
             tot_POI = tot_POI + 1
@@ -42,6 +44,10 @@ def reduceGeoNames(nameFileIn, nameFileOut):
                 tot_pyr = tot_pyr + 1
             if(row[7] == 'HSTS'):
                 tot_hsts = tot_hsts + 1
+            if(row[7] == 'AMTH'):
+                tot_amth = tot_amth + 1
+            if(row[7] == 'BLDG'):
+                tot_bldg = tot_bldg + 1
 
             newRow = row[1] + '\t' + row[4] + '\t' + row[5] + '\t' + row[6] + '\t' + row[7] + '\t' + row[8] + '\n'
             file_out.write(newRow)
@@ -58,6 +64,8 @@ def reduceGeoNames(nameFileIn, nameFileOut):
     print("Tot palaces = ",  tot_pal)
     print("Tot pyramids = ",  tot_pyr)
     print("Tot historical_sites = ",  tot_hsts)
+    print("Tot amphitheaters = ",  tot_amth)
+    print("Tot buildings = ", tot_bldg)
 
     file_out.flush()
     file_out.close()
@@ -67,5 +75,5 @@ def reduceGeoNames(nameFileIn, nameFileOut):
     
     
 
-reduceGeoNames('allCountries.txt', 'reducedGeoNames.txt')
+reduceGeoNames('allCountries.txt', 'reducedGeoNamesNew.txt')
 
