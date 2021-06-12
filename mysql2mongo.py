126c9a860110a9d6794140e620735932eba51d57
Programmer100th <48069809+Programmer100th@users.noreply.github.com>
2021-06-12 10:57:39 +0200

    Updated python file for big data management project

--- mysql2mongo.py
+++ mysql2mongo.py
@@ -1,32 +1,82 @@
 import mysql.connector
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://127.0.0.1:27017/")
-db = client.newDB
-collection = db.newCollection
 
-conn = mysql.connector.connect(user="USERNAME", password="PASSWORD", database='InfIntDB')
+def getGeoviewsData(cur, cid, collection):
+    
+    queryGeo = ("SELECT * FROM geoviews")
+    cur.execute(queryGeo)
 
-cur = conn.cursor(buffered=True)
+    for (name, lon, lat, country, category, relevance) in cur:
+            
+        cus = dict()
+        cid += 1
 
-query = ("SELECT *"
-         "FROM unescoviews")
-cur.execute(query)
+        cus['_id'] = cid
+        cus['name'] = name
+        cus['category'] = category
+        if(country is not None):
+            cus['country'] = country
+        if(relevance is not None):
+            cus['relevance'] = relevance
+        cus['provenance'] = 'GeoNames'
+        location = dict()
+        location['type'] = "Point"
+        location['coordinates'] = [lon, lat]
+        cus['location'] = location
+        collection.insert_one(cus)
 
-cid = 0
-for (name, danger, lon, lat, category, country, relevance) in cur:
-    # print(name,",",danger,",",lon,",",lat,",",category,",",country,",",relevance)
-    cus = dict()
-    cid += 1
+    return cid
 
-    cus['_id'] = cid
-    cus['name'] = name
-    cus['danger'] = danger
-    cus['category'] = category
-    cus['country'] = country
-    cus['relevance'] = relevance
-    location = dict()
-    location['type'] = "Point"
-    location['coordinates'] = [lon, lat]
-    cus['location'] = location
-    collection.insert_one(cus)
+        
+def getUnescoviewsData(cur, cid, collection):
+    
+    queryUnesco = ("SELECT * FROM unescoviews")
+    cur.execute(queryUnesco)
+
+    for (name, danger, lon, lat, category, country, relevance) in cur:
+        
+        cus = dict()
+        cid += 1
+
+        cus['_id'] = cid
+        cus['name'] = name
+        cus['danger'] = danger
+        cus['category'] = category
+        if(country is not None):
+            cus['country'] = country
+        if(relevance is not None):
+            cus['relevance'] = relevance
+        cus['provenance'] = 'Unesco'
+        location = dict()
+        location['type'] = "Point"
+        location['coordinates'] = [lon, lat]
+        cus['location'] = location
+        collection.insert_one(cus)
+        
+    return cid
+
+
+    
+def createMongoDB(mongoClient, userSQL, passwordSQL, databaseSQL):
+
+    client = MongoClient(mongoClient)
+    db = client.bigDataManagementDB
+    collection = db.bigDataManagementCollection
+
+    conn = mysql.connector.connect(user=userSQL, password=passwordSQL, database=databaseSQL)
+
+    cur = conn.cursor(buffered=True)
+
+    cid = 0
+
+    cid = getGeoviewsData(cur, cid, collection)
+
+    cid = getUnescoviewsData(cur, cid, collection)
+
+    print("Cid:", cid)
+
+    
+
+
+createMongoDB("mongodb://127.0.0.1:27017/", 'root', "", 'infintproject') 
